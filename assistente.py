2f28fad51e26e45658ee427a9d51d866b8f04dd6
Lucas Henrique <lucaz.hrq@gmail.com>
2024-04-22 20:03:02 -0300

    Atualização Funções

--- assistente.py
+++ assistente.py
@@ -16,19 +16,16 @@ def ouvir_microfone():
         texto = recognizer.recognize_google(audio, language='pt-BR')
         print("Você disse:", texto)
 
-        # Leitura dos comandos a partir do arquivo JSON
         with open('comandos.json') as json_file:
             comandos = json.load(json_file)['comandos']
         
-        # Tokenização do texto para análise de comandos
-        tokens = word_tokenize(texto.lower())  # Convertendo para minúsculas para padronização
+        tokens = word_tokenize(texto.lower())  
         
-        # Verificar se algum comando reconhecido está presente no texto
         for comando, funcao in comandos.items():
             comando_tokens = word_tokenize(comando.lower())
             if all(token in tokens for token in comando_tokens):
-                # Chamar a função correspondente ao comando
-                globals()[funcao]()  # Chama a função pelo nome
+               
+                globals()[funcao]()  
                 break
         else:
             print("Comando não reconhecido.")
@@ -38,29 +35,22 @@ def ouvir_microfone():
     except sr.RequestError as e:
         print("Erro ao solicitar resultados do serviço de reconhecimento de fala; {0}".format(e))
 
-def nome_assistente():
-    print("Olá, me chamo Jarvis.")
-
 def obter_temperatura_computador():
-    # Simulação da obtenção da temperatura do computador
-    temperatura = random.randint(20, 100)  # Valor de temperatura simulado
+    temperatura = random.randint(20, 100)  
     print("A temperatura do computador é", temperatura, "graus Celsius.")
 
 def porcentagem_processamento():
-    # Gerar uma porcentagem aleatória entre 0 e 100
     porcentagem = random.randint(0, 100)
     print("Porcentagem de processamento:", porcentagem, "%")
 
 
 def tempo_uso_software():
-    # Implemente a lógica para obter o tempo de uso de um determinado software
-    software = "Instagram"  # Software de exemplo
-    tempo_uso = random.randint(0,24)  # Tempo de uso simulado
+    software = "Instagram" 
+    tempo_uso = random.randint(0,24)  
     print("Tempo de uso do software", software + ":", tempo_uso, "horas")
 
 def dispositivos_entrada_conectados():
-    # Implemente a lógica para listar os dispositivos de entrada conectados à máquina
-    dispositivos = ["Mouse", "Teclado", "Webcam"]  # Dispositivos de exemplo
+    dispositivos = ["Mouse", "Teclado", "Webcam"]  
     print("Dispositivos de entrada conectados:", dispositivos)
 
 if __name__ == "__main__":
